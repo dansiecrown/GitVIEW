49e67b90032c4fa8160fa3f80f57f3e751c3d32c
dansiecrown <dansiecrown@gmail.com>
2023-03-14 06:04:33 +0100

    added the 404 route

--- src/router/index.js
+++ src/router/index.js
@@ -41,6 +41,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/DetailView.vue'),    
     },
+    {
+      path: '/:catchAll(.*)',
+      name: 'Error404',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ErrorView.vue')
+    },
   ]
 })
 
