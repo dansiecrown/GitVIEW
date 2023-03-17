bc58f9d731f2fdd9c02c704f64bec59d0aa07855
dansiecrown <dansiecrown@gmail.com>
2023-03-17 17:15:44 +0100

    Still on the error page

--- src/router/index.js
+++ src/router/index.js
@@ -44,7 +44,7 @@ const router = createRouter({
     },
     {
       path: '/:catchAll(.*)*',
-      name: 'Error404',
+      name: 'error',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
